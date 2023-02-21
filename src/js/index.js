9fb72e0cdea3b9cf666d06e640041e474b374198
Landen Doty <landen.doty@outlook.com>
2023-02-21 16:38:22 -0600

    check user input

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,13 @@ let challenges = ["c1", "c2", "c3"];
 function submitChallenge(chalNum) {
     switch(chalNum) {
         case "c1":
-            alert("c1");
+            checkChal1();
             break;
         case "c2":
-            alert("c2");
+            checkChal2();
             break;
         case "c3":
-            alert("c3");
+            checkChal3();
             break;
         default:
     }
@@ -18,13 +18,31 @@ function submitChallenge(chalNum) {
 }
 
 function checkChal1() {
-
+    let submittedVal = document.getElementById("challenge1-text").value; 
+    if(submittedVal.replace(/\s+/g, '').toLowerCase() == atob('cm9jaw==')) {
+        return(true);
+    }
+    else { 
+        return(false); 
+    }
 }
 
 function checkChal2() {
-
+    let submittedVal = document.getElementById("challenge2-text").value; 
+    if(submittedVal.replace(/\s+/g, '').toLowerCase() == atob('Y2hhbGs=')) {
+        return(true);
+    }
+    else { 
+        return(false); 
+    }
 }
 
 function checkChal3() {
-
+    let submittedVal = document.getElementById("challenge3-text").value; 
+    if(submittedVal.replace(/\s+/g, '').toLowerCase() == atob('amF5aGF3aw==')) {
+        return(true);
+    }
+    else { 
+        return(false); 
+    }
 }
